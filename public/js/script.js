8f5fddd1a0fb35881de02895db6150f96cb93d33
IoanGrozniy <idiwpdkfmvjf@gmail.com>
2026-06-21 21:48:55 +0600

    Fixed bug with album search

--- public/js/script.js
+++ public/js/script.js
@@ -37,55 +37,37 @@ async function search() {
         author = Array.isArray(data.author) ? data.author[0] : data.author || null;
         }
         
-        if (type === 'album' || type === 'song') {
+        if (type === 'album') {
             if (!songs || songs.length === 0) {
                 resultsDiv.innerHTML = '<div class="no-results">Nothing was found</div>';
                 return;
             }
             else {
-
+                for (album in albums) {
+                    if (albums[album] && albums[album].cover_url) {
+                        albumCoverDiv.innerHTML += `
+                        <div class="album-cover">
+                        <img src="${albums[album].cover_url}" alt="Album cover">
+                        <p class="album-name">${albums[album].name || ''}</p>
+                        <p class="album-info">Number of songs: ${albums[album].number_of_songs || ''}</p>
+                        `;
+                        albumCoverDiv.innerHTML += songs.filter(s => s.id_album === albums[album].id_album).map(s => `
+                        <div class="song-card">
+                        <div class="song-info">
+                        <h3>${s.name}</h3>
+                        <span>${s.genre || '-'}</span>
+                        </div>
+                        <div>
+                        <a href="${s.youtube_url}" target="_blank" class="listen-btn">▶ Play</a>
+                        </div>
+                        </div>
+                        `).join('');
+                    }
+                albumCoverDiv.innerHTML += `<br>`;
+                }
             }
-        } else if (type === 'author') {
-            if (!albums || albums.length === 0) {
-                resultsDiv.innerHTML = '<div class="no-results">Nothing was found</div>';
-                return;
-            }
-        } 
-        
-        if (author && author.photo_url) {
-    artistPhotoDiv.innerHTML = `
-        <div class="artist-photo">
-            <img src="${author.photo_url}" alt="Artist's photo">
-            <p class="artist-name">${author.name || ''}</p>
-            <p class="artist-career">Career started in ${author.career_start_year || ''}</p>
-        </div>
-    `;
-}
-
-        for (album in albums) {
-            if (albums[album] && albums[album].cover_url) {
-                albumCoverDiv.innerHTML += `
-                <div class="album-cover">
-                <img src="${albums[album].cover_url}" alt="Album cover">
-                <p class="album-name">${albums[album].name || ''}</p>
-                <p class="album-info">Number of songs: ${albums[album].number_of_songs || ''}</p>
-            `;
-            albumCoverDiv.innerHTML += songs.filter(s => s.id_album === albums[album].id_album).map(s => `
-            <div class="song-card">
-            <div class="song-info">
-            <h3>${s.name}</h3>
-            <span>${s.genre || '-'}</span>
-            </div>
-            <div>
-            <a href="${s.youtube_url}" target="_blank" class="listen-btn">▶ Play</a>
-            </div>
-            </div>
-            `).join('');
-            }
-            albumCoverDiv.innerHTML += `<br>`;
-        }
-        
-        resultsDiv.innerHTML = songs.map(s => `
+        } else if (type === 'song') {
+            resultsDiv.innerHTML = songs.map(s => `
             <div class="song-card">
                 <div class="song-info">
                     <h3>${s.name}</h3>
@@ -96,6 +78,35 @@ async function search() {
                 </div>
             </div>
         `).join('');
+        } 
+        else if (type === 'author') {
+            if (!albums || albums.length === 0) {
+                resultsDiv.innerHTML = '<div class="no-results">Nothing was found</div>';
+                return;
+            }
+            else {
+                for (album in albums) {
+                    if (albums[album] && albums[album].cover_url) {
+                        albumCoverDiv.innerHTML += `
+                        <div class="album-cover">
+                        <img src="${albums[album].cover_url}" alt="Album cover">
+                        <p class="album-name">${albums[album].name || ''}</p>
+                        <p class="album-info">Number of songs: ${albums[album].number_of_songs || ''}</p>
+                        `;
+                    }
+                }   
+            }
+        } 
+        
+        if (author && author.photo_url) {
+            artistPhotoDiv.innerHTML = `
+            <div class="artist-photo">
+            <img src="${author.photo_url}" alt="Artist's photo">
+            <p class="artist-name">${author.name || ''}</p>
+            <p class="artist-career">Career started in ${author.career_start_year || ''}</p>
+            </div>
+            `;
+        }
         
     } catch (e) {
         console.error(e);
